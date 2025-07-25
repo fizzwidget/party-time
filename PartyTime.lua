f71cd42c5e144ca40fafc4a6450e59e0d544effd
Sethenot <sethenot@fizzwidget.com>
2025-07-25 16:10:09 -0700

    Tooltip design without function

--- PartyTime.lua
+++ PartyTime.lua
@@ -101,13 +101,7 @@ SlashCmdList["PARTYTIME"] = T.ChatCommandHandler
 -- TEMP
 SLASH_PARTYQUEST1 = "/pq"
 SlashCmdList["PARTYQUEST"] = function(text)
-	if text == "add" then
-		C_ChatInfo.SendAddonMessage(addonName, "Q|ADD 1234", "PARTY")
-	elseif text == "remove" then
-		C_ChatInfo.SendAddonMessage(addonName, "Q|REMOVE 1234", "PARTY")
-	elseif text == "" then
-		DevTools_Dump(T.TrackedQuests)
-	end
+	DevTools_Dump(T.TrackedQuests)
 end
 
 local function questLogMenu(owner, rootDescription, contextData)
@@ -157,8 +151,28 @@ function T.ShowFrame()
 	T.Frame:SetPadding(T.Frame.CloseButton:GetWidth() + 2, 0)
 	for id in pairs(T.TrackedQuests) do
 		GameTooltip_AddNormalLine(T.Frame, C_QuestLog.GetTitleForQuestID(id), false)
+		GameTooltip_AddHighlightLine(T.Frame, x("player"):WrapTextInColorCode(y(UnitName("player"))).." "..x("party1"):WrapTextInColorCode(y(UnitName("party1"))).." "..x("party2"):WrapTextInColorCode(y(UnitName("party2"))).." "..x("party3"):WrapTextInColorCode(y(UnitName("party3"))).." "..x("party4"):WrapTextInColorCode(y(UnitName("party4"))), false)
 	end
 	T.Frame:Show()
+end
+
+function x(input)
+	if UnitName(input) == "Sethenot" then
+		return GREEN_FONT_COLOR
+	elseif UnitClass(input) == "Rogue" then
+		return WHITE_FONT_COLOR
+	else
+		return RED_FONT_COLOR
+	end
+end
+
+function y(input)
+	if input == nil then
+		input = ""
+		return input
+	else
+		return input
+	end
 end
 
 T.ShowFrame()
